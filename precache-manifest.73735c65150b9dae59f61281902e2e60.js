c8de4ac462e2f4c352b13119a4c114b2825144fe
Kaan Seyitogullari <kaan.seyitogullari@zalando.ie>
2020-01-25 18:03:07 +0000

    Updates

---
+++ precache-manifest.73735c65150b9dae59f61281902e2e60.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1b0e612c6a4aacec4dc5354f5112b6",
+    "url": "/react-view-tracker/index.html"
+  },
+  {
+    "revision": "6fd4d7d8bd9f50df0944",
+    "url": "/react-view-tracker/static/js/2.9ae2057b.chunk.js"
+  },
+  {
+    "revision": "f4b987e6ea9d313ac72e",
+    "url": "/react-view-tracker/static/js/main.9df76a01.chunk.js"
+  },
+  {
+    "revision": "638114d86e45619d6b64",
+    "url": "/react-view-tracker/static/js/runtime-main.f3e58476.js"
+  }
+]);
